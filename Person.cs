05bea97e1f43cfd57cf95cb7727b93ec4163e111
Fabrizio <Petrozzi@usf.edu>
2025-09-30 21:29:47 -0400

    add Person.cs

---
+++ Person.cs
@@ -0,0 +1,16 @@
+namespace USFLibraryManagement
+{
+        public class Person
+    {
+        public string Name;
+        public string Email;
+        public string ID;
+
+        public Person()
+        {
+            Name = "";
+            Email = "";
+            ID = "";
+        }
+    }
+}
